58f4531f4ae1d91a020af37fd4939e24967e0d48
mitchel shephard <miitchel@hotmail.co.uk>
2023-04-06 09:02:59 +0100

    Completion of random file

--- HeadsOrTails/HeadsOrTails.py
+++ HeadsOrTails/HeadsOrTails.py
@@ -1,5 +1,7 @@
 import random
 
+print("Hello this is my random integer program")
 random_integer = random.randint(1,10)
+random_float = random.random()
 print(random_integer)
-
+print(f"My random integer is {random_integer}, and my random float is {random_float}")
