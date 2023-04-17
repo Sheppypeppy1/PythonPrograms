7e5edd00430edd76c5229d73963ca372d07cdbe6
mitchel shephard <miitchel@hotmail.co.uk>
2023-04-17 21:36:56 +0100

    added day 32 project. connected to google account and sends email from python terminal

---
+++ BirthdayWisher/main.py
@@ -0,0 +1,13 @@
+import smtplib
+
+with open('data.txt','r') as file:
+    data = file.readlines()
+
+my_email = data[0]
+my_password = data[1]
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(from_addr=my_email,to_addrs=my_email, 
+                        msg="Subject:Hello\n\nThis is the body of my email")
